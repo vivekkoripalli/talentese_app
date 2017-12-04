7d841a2be7488ad935f2b42654ea7cd62e1a179d
vivekkoripalli <vivek.koripalli@gmail.com>
2017-12-04 12:24:35 +0530

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'pages/explorecompanies'
+
+  get 'pages/jobs'
+
+  get 'pages/coaches'
+
+  get 'pages/Adivce'
+
+  get 'pages/forcompanies'
+
+
+  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
